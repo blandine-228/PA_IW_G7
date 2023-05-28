13a2942c14450885cb9e0e62521b396f0a6f9b78
Jp Hayek <jphayek1@gmail.com>
2023-05-28 22:56:27 +0200

    modif view dashboard

--- www/Views/DashboardView.php
+++ www/Views/DashboardView.php
@@ -6,14 +6,18 @@
 </head>
 <body>
     <div class="container">
-        <h1>Tableau de bord</h1>
+        <h1>Dashboard</h1>
         <div class="dashboard">
-            <?php foreach ($data as $item): ?>
-                <div class="widget">
-                    <h2><?php echo $item['title']; ?></h2>
-                    <p><?php echo $item['content']; ?></p>
-                </div>
-            <?php endforeach; ?>
+            <?php if (!empty($data)): ?>
+                <?php foreach ($data as $item): ?>
+                    <div class="widget">
+                        <h2><?php echo $item['title']; ?></h2>
+                        <p><?php echo $item['content']; ?></p>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>Aucune donnée disponible pour le tableau de bord.</p>
+            <?php endif; ?>
         </div>
     </div>
 </body>
