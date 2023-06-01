04977a0b86cbff6647f1734c10cc826465af4355
Jp Hayek <jphayek1@gmail.com>
2023-06-01 10:05:58 +0200

    db modif + creating page

--- database.sql
+++ database.sql
@@ -1,4 +1,73 @@
 -- Adminer 4.8.1 PostgreSQL 15.2 (Debian 15.2-1.pgdg110+1) dump
+-- Adminer 4.8.1 PostgreSQL 15.2 (Debian 15.2-1.pgdg110+1) dump
+
+DROP TABLE IF EXISTS "Address";
+CREATE TABLE "public"."Address" (
+    "user_id" character(1) NOT NULL,
+    "name" character(1) NOT NULL,
+    "address" text NOT NULL,
+    "district" text NOT NULL,
+    "city" text NOT NULL,
+    "postal_code" numeric NOT NULL,
+    "phone" numeric NOT NULL,
+    "updated_at" timestamp NOT NULL
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "Image";
+CREATE TABLE "public"."Image" (
+    "file_name" character(1) NOT NULL,
+    "user_id" uuid,
+    "created_at" timestamp NOT NULL
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "Menu";
+CREATE TABLE "public"."Menu" (
+    "title" text NOT NULL,
+    "description" text NOT NULL,
+    "image" path,
+    "created_at" timestamp NOT NULL,
+    "updated_at" timestamp NOT NULL,
+    "active" integer NOT NULL
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "Page";
+CREATE TABLE "public"."Page" (
+    "title" text NOT NULL,
+    "image" path,
+    "html" xml NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "updated_at" timestamp NOT NULL,
+    "active" integer NOT NULL
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "Product";
+CREATE TABLE "public"."Product" (
+    "name" character(1) NOT NULL,
+    "description" character varying NOT NULL,
+    "quantity" numeric NOT NULL,
+    "price" integer NOT NULL,
+    "image" path NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "updated_at" timestamp NOT NULL
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "User";
+CREATE TABLE "public"."User" (
+    "firstname" character varying NOT NULL,
+    "lastname" character varying NOT NULL,
+    "email" character varying NOT NULL,
+    "pwd" character varying NOT NULL,
+    "country" character varying NOT NULL,
+    "status" integer,
+    "date_inserted" timestamp NOT NULL,
+    "date_updated" timestamp NOT NULL
+) WITH (oids = false);
+
 
 DROP TABLE IF EXISTS "article_category";
 CREATE TABLE "public"."article_category" (
@@ -22,62 +91,9 @@ CREATE TABLE "public"."articles" (
 ) WITH (oids = false);
 
 
-DROP TABLE IF EXISTS "categories";
-DROP SEQUENCE IF EXISTS categories_id_seq;
-CREATE SEQUENCE categories_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."categories" (
-    "id" integer DEFAULT nextval('categories_id_seq') NOT NULL,
-    "name" character varying(255) NOT NULL,
-    "description" text,
-    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "comments";
-DROP SEQUENCE IF EXISTS comments_id_seq;
-CREATE SEQUENCE comments_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."comments" (
-    "id" integer DEFAULT nextval('comments_id_seq') NOT NULL,
-    "content" text,
-    "user_id" integer NOT NULL,
-    "page_id" integer NOT NULL,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "pages";
-DROP SEQUENCE IF EXISTS pages_id_seq;
-CREATE SEQUENCE pages_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."pages" (
-    "id" integer DEFAULT nextval('pages_id_seq') NOT NULL,
-    "title" character varying(255) NOT NULL,
-    "content" text,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "pages_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-
-DROP TABLE IF EXISTS "users";
-DROP SEQUENCE IF EXISTS users_id_seq;
-CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."users" (
-    "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
-    "username" character varying(255) NOT NULL,
-    "email" character varying(255) NOT NULL,
-    "password" character varying(255) NOT NULL,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-
 ALTER TABLE ONLY "public"."article_category" ADD CONSTRAINT "article_category_article_id_fkey" FOREIGN KEY (article_id) REFERENCES articles(id) NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."article_category" ADD CONSTRAINT "article_category_category_id_fkey" FOREIGN KEY (category_id) REFERENCES categories(id) NOT DEFERRABLE;
 
 ALTER TABLE ONLY "public"."articles" ADD CONSTRAINT "articles_author_id_fkey" FOREIGN KEY (author_id) REFERENCES users(id) NOT DEFERRABLE;
 
--- 2023-06-01 06:47:44.567421+00
+-- 2023-06-01 08:05:19.103674+00
