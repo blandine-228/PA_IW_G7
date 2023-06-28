f3ffe34b3245d534d9a9cc6279b4bfd058af52ec
GootPyke <gauthiers.mail@gmail.com>
2023-06-28 18:07:04 +0200

    Changement du index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,14 @@
 namespace App;
 
 //require "Core/View.php";
+define("WWW_PATH", getenv("WWW_PATH"));
+//Set database consts
+define("DB_TYPE", getenv("DB_TYPE"));
+define("DB_HOST", getenv("DB_HOST"));
+define("DB_NAME", getenv("DB_NAME"));
+define("DB_PORT", getenv("DB_PORT"));
+define("DB_USERNAME", getenv("DB_USERNAME"));
+define("DB_PASSWORD", getenv("DB_PASSWORD"));
 
 spl_autoload_register(function ($class) {
     //$class = App\Core\View
