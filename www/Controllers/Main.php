796c4efda6bfbfe1a54ebe3a71afb0999cdb2258
Lisa Guemmar <guemmarlisa@gmail.com>
2023-07-06 22:27:54 +0200

    affichage des aarticles sur le front

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -3,17 +3,20 @@
 namespace App\Controllers;
 
 use App\Core\View;
+use App\Models\Article as ArticleModel;
+//use App\datatable\articleTable;
+use App\datatable\articleTableFront;
 
 class Main
 {
-    public function home(): void
+   /* public function home(): void
     {
         $pseudo = "Prof";
         $view = new View("Main/home", "front");
         $view->assign("pseudo", $pseudo);
         $view->assign("age", 30);
         $view->assign("titleseo", "supernouvellepage");
-    }
+    }*/
 
     public function contact(): void
     {
@@ -23,6 +26,21 @@ class Main
     public function aboutUs(): void
     {
         echo "Page à propos";
+    }
+
+    public function home()
+    {
+         // Récupérer tous les articles depuis la base de données
+         $article = ArticleModel::getInstance();
+         $allArticles = $article->getAll();
+         // Afficher les articles dans une vue appropriée
+         $table = new articleTableFront($allArticles);
+ 
+        // $view = new View('Article/read', 'back');
+       
+         $view = new View("Main/home", "front");
+         $view->assign('table', $table->getConfig($allArticles));
+         //$view->render()
     }
 
 }
--- www/Views/Main/home.view.php
+++ www/Views/Main/home.view.php
@@ -1,2 +1,4 @@
-<h2>Welcome <?= $pseudo; ?> </h2>
-<h3><?= $age?></h3>
+<h1>Page d'accueil</h1>
+<?php $this->partial("table", $table); ?>
+
+<!--<li><a href="/read_article">Afficher les articles</a></li>-->
