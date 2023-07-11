28ddd81a5d1b23ca348f6c27e91a366169e07e31
GootPyke <gauthiers.mail@gmail.com>
2023-07-11 14:42:34 +0200

    Ajout de mes modifs

--- www/Views/Main/home.view.php
+++ www/Views/Main/home.view.php
@@ -1,2 +1,10 @@
-<h2>Welcome <?= $pseudo; ?> </h2>
-<h3><?= $age?></h3>
+<main role="main" class="flex-1">
+    <!-- Contenu de la page -->
+    <div class="container mx-auto mt-16">
+        <h1 class="text-4xl font-bold mb-8">Bienvenue sur notre site !</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec elit risus. Suspendisse vitae lacinia nunc,
+            vitae interdum enim. Quisque nec nunc ligula. Phasellus posuere lacinia nisl ac sollicitudin. Morbi eget mi
+            sed dui laoreet tristique nec eget ligula. Fusce non tincidunt risus, sit amet bibendum mi. Sed nec risus
+            nec mi finibus tempor.</p>
+    </div>
+</main>
--- www/Views/Templates/back.tpl.php
+++ www/Views/Templates/back.tpl.php
@@ -8,29 +8,7 @@
     <script src="https://code.highcharts.com/highcharts.js"></script>
 </head>
 <body class="grid grid-cols-12 h-screen">
-    <aside class="bg-white text-gray-200 flex flex-col py-8 px-6 col-span-3 ">
-        <div class="py-1 px-6">
-            <h1 class="text-sm uppercase font-semibold text-slate-700">Accueil</h1>
-        </div>
-        <nav class="px-6 py-1">
-            <a href="#" class="block text-slate-700">Tableau de bord</a>
-        </nav>
-
-        <hr class="my-4 md:min-w-full">
-
-        <div class="py-1 px-6">
-            <h1 class="block py-2 uppercase text-sm font-semibold text-slate-700">Personnalisation</h1>
-        </div>
-        <nav class="py-1 px-6">
-            <a href="#" class="block pb-3 text-slate-700">Menu du site</a>
-            <a href="#" class="block pb-3 text-slate-700">Couleurs principales du site</a>
-            <a href="#" class="block pb-3 text-slate-700">Typographies</a>
-        </nav>
-    </aside>
-
-    <main role="main" class="flex justify-center items-center bg-white col-span-9">
-        <!-- inclure la vue -->
-        <?php include $this->view;?>
-    </main>
+    <!-- inclure la vue -->
+    <?php include $this->view;?>
 </body>
 </html>
